94814821d69b23cb774564084419e1900f735ee6
gros-pataplouf <sceres@pm.me>
2024-05-21 21:40:36 +0200

    test: one ingredient cake - same quantity as recipe

---
+++ baker.py
@@ -0,0 +1,2 @@
+def cake(ingredients, recipe):
+    return list(ingredients.values())[0]/list(recipe.values())[0]
---
+++ test_baker.py
@@ -0,0 +1,15 @@
+from  baker import cake
+
+# case 1: one ingredient cake, recipe only for one ingredient same quantity
+# case 1a: one ingredient cake, recipe only for one ingredient same quantity, division result is float > 0
+# case 1b: one ingredient cake, recipe only for one ingredient same quantity, division result is float > 0
+# case 2: one ingredient cake, recipe only another ingredient
+# case 3: cake with several ingredients, recipe with several ingredients, partial overlap, cake not possible
+# case 4: cake with several ingredients, recipe with several, recipe is subs. of ingredients, cake possible
+
+
+def test_cake_only_one_ingredient():
+    ingredients = {"flour": 500}
+    recipe = {"flour": 500}
+    result = cake(ingredients, recipe)
+    assert result == 1
