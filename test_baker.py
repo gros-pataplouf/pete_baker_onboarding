ccecebd0bce195c7fc690a9416bcd5cf2924f9d6
gros-pataplouf <sceres@pm.me>
2024-05-25 22:47:57 +0200

    test: rewrite test takes two arguments

--- test_baker.py
+++ test_baker.py
@@ -1,24 +1,12 @@
 import pytest
 from  baker import cake
 
-# tests: takes two objects / dictionaires
-# unavailable ingredient considered a 0
-# returns zero if at least one ingredient in insufficient quantity 
-# throws if 
-# wrong number of arguments 
-
-"""
-Can you help him to find out, how many cakes he could bake considering his recipes?
-Write a function cakes(), which takes the recipe (object) and the available ingredients(also an object)
-and returns the maximum number of cakes Pete can bake (integer).
-For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
-Ingredients that are not present in the objects, can be considered as 0.
-Examples: // must return 2 cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); // must return 0 cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
-"""
 
 def test_cake_takes_two_args():
-    cake({"flour": 400}, {"milk": 200})
-    assert True
+    try:
+        cake({"flour": 400}, {"milk": 200})
+    except Exception:
+        assert False
 
 def test_throws_if_args_not_dicts():
     with pytest.raises(TypeError) as e:
