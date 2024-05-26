9c959dbc7d3bd40dff8306851cb1894a46273992
gros-pataplouf <sceres@pm.me>
2024-05-26 08:54:54 +0200

    refactor: remove unnecessary check for availability

--- baker.py
+++ baker.py
@@ -1,12 +1,5 @@
-def cake(*args):
-    if len(args) != 2:
-        raise Exception("This function takes exactely two arguments")
-    for arg in args:
+def cake(available: object, recipe: object) -> int:
+    for arg in [available, recipe]:
         if not isinstance(arg, dict):
             raise TypeError("Arguments must be dictionaries.")
-    available, recipe  = args
-    recipe_ingredients = set(recipe.keys())
-    available_ingredients = set(available.keys())
-    if not recipe_ingredients.issubset(available_ingredients):
-        return 0
-    return min([available[ingredient] // recipe[ingredient] for ingredient in recipe_ingredients])
+    return min([available.get(ingredient, 0) // recipe[ingredient] for ingredient in recipe.keys()])
