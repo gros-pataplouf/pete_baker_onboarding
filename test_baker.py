345f2d20153fe9440e4f08aeae521828c1b1c20f
gros-pataplouf <sceres@pm.me>
2024-06-04 16:08:27 +0200

    test: round down to least available ingredient

--- test_baker.py
+++ test_baker.py
@@ -84,22 +84,12 @@ def test_returns_3_if_triple_of_recipe():
     result = petes_bakery.max_cakes("Apple Cake")
     assert result == 3
 
+def test_rounds_down_to_least_available_ingredient():
+    petes_bakery = Bakery("Pete's Delights")
+    petes_bakery += {"flour": 1000, "cocoa powder": 500, "apples": 9, "flour": 900, "butter": 400}
+    applecake_recipe = Recipe("Apple Cake", {"apples": 3, "flour": 300, "butter": 400})
+    petes_bakery.recipes.append(applecake_recipe)
+    result = petes_bakery.max_cakes("Apple Cake")
+    assert result == 1
 
-# def test_returns_0_if_ingredient_insufficient():
-#     available = {"sugar": 500}
-#     recipe = {"sugar": 600}
-#     result = cake(available, recipe)
-#     assert result == 0
-
-# def test_returns_num_of_cakes_for_several_ingredients():
-#     available = {"sugar": 500, "milk": 250, "flour": 700, "eggs": 12}
-#     recipe = {"sugar": 100, "milk": 100, "flour": 70, "eggs": 1}
-#     result = cake(available, recipe)
-#     assert result == 2
-
-# def test_ingredient_order_does_not_matter():
-#     available = {"sugar": 500, "milk": 250, "flour": 700, "eggs": 12}
-#     recipe = {"milk": 100, "eggs": 1, "flour": 70, "sugar": 100}
-#     result = cake(available, recipe)
-#     assert result == 2
 
