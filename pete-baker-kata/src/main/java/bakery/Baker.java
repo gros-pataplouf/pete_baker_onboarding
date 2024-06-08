0b7cecdc74d5817840f1c0f02267effe029ef12c
gros-pataplouf <sceres@pm.me>
2024-06-08 12:15:22 +0200

    test: cake function returns 1

--- pete-baker-kata/src/main/java/bakery/Baker.java
+++ pete-baker-kata/src/main/java/bakery/Baker.java
@@ -1,13 +1,18 @@
 package bakery;
+import java.util.HashMap;
+
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class Baker 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    public static int cakes(HashMap available, HashMap recipe) {
+        return 1;
     }
 }
---
+++ pete-baker-kata/src/test/java/bakery/BakerTest.java
@@ -0,0 +1,30 @@
+package bakery;
+
+import java.util.HashMap;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BakerTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void cakeTestReturns1()
+    {
+        HashMap<String, Integer> recipe = new HashMap<>();
+        recipe.put("flour", 200);
+        HashMap<String, Integer> available = new HashMap<>();
+        available.put("flour", 200);
+        Baker pete = new Baker();
+        int numOfCakes = pete.cakes(available, recipe);
+        assertTrue(numOfCakes == 1);
+
+    }
+}
